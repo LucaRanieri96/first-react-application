ba3278511fa34f32f5f708f40b04e421a5e38627
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-08-31 17:26:55 +0200

    Add dinamic class onClick

--- react-app1/src/components/ListGroup.tsx
+++ react-app1/src/components/ListGroup.tsx
@@ -1,18 +1,34 @@
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
 function ListGroup() {
-let items = [
-  'Milano',
-  'Firenze',
-  'Roma',
-  'Napoli',
-  'Catania',
-];
+  let items = ["Milano", "Firenze", "Roma", "Napoli", "Catania"];
+  // Hook
+  let [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
-      { items.length === 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-       {items.map((item, index) => <li key={item} className="list-group-item" onClick={()=> console.log(item, index)}>{item}</li>)}
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );

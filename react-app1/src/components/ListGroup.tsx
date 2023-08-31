280a680755f9e4a5df4b3c0deebc8bf61b7ac853
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-08-31 17:09:53 +0200

    see onClick behaviour

--- react-app1/src/components/ListGroup.tsx
+++ react-app1/src/components/ListGroup.tsx
@@ -7,14 +7,12 @@ let items = [
   'Catania',
 ];
 
-items = [];
-
   return (
     <>
       <h1>List</h1>
       { items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-       {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+       {items.map((item, index) => <li key={item} className="list-group-item" onClick={()=> console.log(item, index)}>{item}</li>)}
       </ul>
     </>
   );
